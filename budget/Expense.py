2966055b07e32746b1a7e3553eb0421c78f9a7ee
Vinaykumar Reddy Kakanuru <31994314+vinaykakanuru@users.noreply.github.com>
2022-12-01 22:39:09 +0530

    Completed Module1

--- budget/Expense.py
+++ budget/Expense.py
@@ -30,13 +30,13 @@ class Expenses():
             food_expenses = set()
             unnecessary_expenses = set()
             for i in self.list:
-                if (i.category == 'Phone'    or i.category == 'Auto and Gas' or 
-                    i.category == 'Classes'  or i.category == 'Utilities' or 
-                    i.category == 'Mortgage'): 
+                if (i.category == 'Phone'    or i.category == 'Auto and Gas' or
+                    i.category == 'Classes'  or i.category == 'Utilities' or
+                    i.category == 'Mortgage'):
                     necessary_expenses.add(i)
                 elif(i.category == 'Groceries' or i.category == 'Eating Out'):
                     food_expenses.add(i)
                 else:
                     unnecessary_expenses.add(i)
-            
+
             return [necessary_expenses, food_expenses, unnecessary_expenses]
